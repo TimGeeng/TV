21c63a1cf99d3119e4018dc65ef842a38dd19469
TimGeeng <68497505+TimGeeng@users.noreply.github.com>
2024-03-22 12:27:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 source_file = "demo.txt"
-final_file = "result.txt"
+final_file = "channel.txt"
 favorite_list = [
-    "广东珠江",
-    "开平综合",
-    "开平生活",
     "CCTV-1",
     "CCTV-5",
     "CCTV-5+",
     "CCTV-13",
     "广东体育",
-    "广东卫视",
     "大湾区卫视",
     "浙江卫视",
     "湖南卫视",
